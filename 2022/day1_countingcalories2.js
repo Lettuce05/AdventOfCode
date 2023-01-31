25868f7d57d4226636b9554edb970c3cccf4110d
Anthony Romaine <anthony.romaine1@gmail.com>
2023-01-31 05:17:49 +0000

    Fix edge case for no newline at end of file

--- 2022/day1_countingcalories2.js
+++ 2022/day1_countingcalories2.js
@@ -19,4 +19,15 @@ while (line = calorieLines.next()) {
     calorieCount = 0;
   }
 }
+
+// edge case for last set of numbers (since there is no empty line at end of file)
+if (calorieCount > 0) {
+  for (max in maxCalories) {
+    if (calorieCount > maxCalories[max]) {
+      maxCalories.splice(max, 0, calorieCount);
+      maxCalories.pop()
+      break;
+    }
+  }
+}
 console.log(maxCalories[0] + maxCalories[1] + maxCalories[2]);
